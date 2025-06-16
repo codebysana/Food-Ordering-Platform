18a1f822f8c7824f462fd2f7e1a054facbb4eed7
codebysana <sana.younis05@gmail.com>
2025-06-16 15:39:37 +0500

    Work Complete

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <div className="border-b-2 border-b-orange-500 py-6">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500 px-10">
+        <Link
+          to="/"
+          className="text-3xl font-bold tracking-tight text-orange-500 px-10"
+        >
           BiteKart
         </Link>
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
       </div>
     </div>
   );
