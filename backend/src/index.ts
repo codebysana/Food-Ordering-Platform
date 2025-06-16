42bf018ba626c78646bcb33d62ad60f3d19df531
codebysana <sana.younis05@gmail.com>
2025-06-16 12:01:09 +0500

    Work Complete

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello" });
+});
+
+app.listen(7000, () => {
+  console.log("Server started on localhost:7000");
+});
+
+
+// db user name and password
+// username: admin
+// password: V054ykbMGJlb9zqr
