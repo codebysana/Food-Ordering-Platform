efd5e43cc3961b66cdb57cfe1886eaf837247a8a
codebysana <sana.younis05@gmail.com>
2025-06-25 20:46:28 +0500

    Work Complete

---
+++ backend/src/routes/restaurantRoute.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { fixHandler } from "../../utils/fixHandler";
+import multer from "multer";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+});
+
+// api/my/restaurant
+router.post(
+  "/",
+  upload.single("imageFile"),
+  fixHandler(myRestaurantController.createMyRestaurant)
+);
