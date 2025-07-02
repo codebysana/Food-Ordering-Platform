7303e10a2995cf5d4fca2ab8df6508fbb73c5673
codebysana <sana.younis05@gmail.com>
2025-07-02 10:36:51 +0500

    Work Complete

--- frontend/src/api/RestaurantApi.tsx
+++ frontend/src/api/RestaurantApi.tsx
@@ -1,6 +1,28 @@
 import type { SearchState } from "@/pages/SearchPage";
-import type { RestaurantSearchResponse } from "@/types";
+import type { Restaurant, RestaurantSearchResponse } from "@/types";
 import { useQuery } from "react-query";
+
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetRestaurant = (restaurantId?: string) => {
+  const getRestaurantByIdRequest = async (): Promise<Restaurant> => {
+    const response = await fetch(
+      `http://localhost:5000/api/restaurant/${restaurantId}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to get restaurant");
+    }
+    return response.json();
+  };
+  const { data: restaurant, isLoading } = useQuery(
+    "fetchRestaurant",
+    getRestaurantByIdRequest,
+    {
+      enabled: !!restaurantId,
+    }
+  );
+  return { restaurant, isLoading };
+};
 
 export const useSearchRestaurants = (
   searchState: SearchState,
---
+++ frontend/src/pages/DetailPage.tsx
@@ -0,0 +1,32 @@
+import { useGetRestaurant } from "@/api/RestaurantApi";
+import RestaurantInfo from "@/components/RestaurantInfo";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { useParams } from "react-router-dom";
+
+const DetailPage = () => {
+  const { restaurantId } = useParams();
+  const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+
+  if (isLoading || !restaurant) {
+    return "Loading...";
+  }
+
+  return (
+    <div className="flex flex-col gap-10">
+      <AspectRatio ratio={16 / 5}>
+        <img
+          src={restaurant.imageUrl}
+          alt=""
+          className="rounded-md object-cover h-full w-full"
+        />
+      </AspectRatio>
+      <div className="grid d:grid-cols-[4fr_2fr] gap-5 md:px-32">
+        <div className="flex flex-col gap-4">
+          <RestaurantInfo restaurant={restaurant} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailPage;
