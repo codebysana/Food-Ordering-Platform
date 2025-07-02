0e51c7ca06c9f214d9c370c3a5c9d990a5dc3e91
codebysana <sana.younis05@gmail.com>
2025-07-02 10:44:58 +0500

    Work Complete

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -7,7 +7,7 @@ export type User = {
   country: string;
 };
 
-export type MenuItemType = {
+export type MenuItem = {
   _id: string;
   name: string;
   price: number;
@@ -22,7 +22,7 @@ export type Restaurant = {
   deliveryPrice: number;
   estimatedDeliveryTime: number;
   cuisines: string[];
-  menuItems: MenuItemType[];
+  menuItems: MenuItem[];
   imageUrl: string;
   lastUpdated: string;
 };
