b5890f40e8e8dda527af40fd89cf437896492143
codebysana <sana.younis05@gmail.com>
2025-07-03 23:00:38 +0500

    Work Complete

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 import { Request, Response } from "express";
-import Restaurant from "../models/restaurant";
+import Restaurant, { MenuItemType } from "../models/restaurant";
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -29,10 +29,29 @@ const createCheckoutSession = async (req: Request, res: Response) => {
     if (!restaurant) {
       throw new Error("Restaurant not found");
     }
+    const lineItems = createLineItems(
+      checkoutSessionRequest,
+      restaurant.menuItems
+    );
   } catch (error: any) {
     console.log(error);
     res.status(500).json({ message: error.raw.message });
   }
+};
+
+const createLineItems = (
+  checkoutSessionRequest: CheckoutSessionRequest,
+  menuItems: MenuItemType[]
+) => {
+  // 1. foreach cartItem, get the menuItem object from the restaurant
+  // 2. foreach cartItem, covert it to a stripe line item
+  // 3. return line item array
+
+  const lineItems = checkoutSessionRequest.cartItems.map((cartItem) => {
+    const menuItem = menuItems.find(
+      (item) => item._id.toString() === cartItem.menuItemId.toString()
+    );
+  });
 };
 
 export default {
